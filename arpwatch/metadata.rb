c4f03f7f7b03c8e83d92303be02530f23b4d00b3
keegan <keeganh@aweber.com>
2013-12-23 14:22:12 -0500

    version bump

--- arpwatch/metadata.rb
+++ arpwatch/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'keeganh@aweber.com'
 license          'BSD 3 clause'
 description      'Installs arpwatch from package'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 supports          'ubuntu'
 supports          'debian'
 
